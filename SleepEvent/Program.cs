67e62fdcf7766387c44c5ba13cf8f9c3485c7076
Volha Kuralenia <Volha_Kuralenia@epam.com>
2021-06-06 21:41:25 +0300

    event refactoring

--- SleepEvent/Program.cs
+++ SleepEvent/Program.cs
@@ -7,50 +7,62 @@ namespace SleepEvent
     class Timer
     {
         private int seconds;
-        private TimerCount timerCount;
-        public delegate void TimerNotify(string t);
-        public event TimerNotify Notify;
+        public event TimerCount TimerCount;
+        //public delegate void TimerNotify(string t);
+
+        //public event TimerNotify Notify;
         public Timer(int s)
         {
             seconds = s;
         }
-        public TimerCount TimerCount
+        /*public TimerCount TimerCount
         {
-            get { return timerCount; }
             set { timerCount = value; }
+            get { return timerCount; }
         }
+        */
         public void CountBackward()
         {
-            Console.WriteLine("Counting started...");
             while (seconds > 0)
             {
-                timerCount(seconds--);
+                TimerCount(seconds--);
                 Thread.Sleep(1000);
             }
-            Notify.Invoke("It's time!");
+            //Notify.Invoke("It's time!");
         }
     }
     class Program
     {
-        private static void ShowMessage(string txt)
+       /* private static void ShowMessage(string txt)
         {
             Console.WriteLine(txt);
         }
+       */
         private static Timer InputSeconds()
         {
             Console.WriteLine("Enter seconds for counting:");
             int seconds = Convert.ToInt32(Console.ReadLine());
+
             return new Timer(seconds);
         }
         static void Main()
         {
             Timer timer = InputSeconds();
-            timer.Notify += ShowMessage;
+            //timer.Notify += ShowMessage;
             TimerCount timerCountMain = (n) =>
             {
                 Console.WriteLine(n);
             };
-            timer.TimerCount = timerCountMain;
+            TimerCount Each10Sec = (n) =>
+            {
+                if (n % 10 == 0)
+                {
+                    Console.WriteLine(n + " passed...");
+                }
+            };
+            timer.TimerCount += timerCountMain;
+            timer.TimerCount += Each10Sec;
+            Console.WriteLine("Counting started...");
             timer.CountBackward();
         }
     }
