abfc20bcd745428c658254cb411e30904e54eea6
Volha Kuralenia <Volha_Kuralenia@epam.com>
2021-06-15 18:02:18 +0300

    Event class based on EventArgs added

--- SleepEvent/Program.cs
+++ SleepEvent/Program.cs
@@ -3,25 +3,39 @@ using System.Threading;
 
 namespace SleepEvent
 {
+    class MessageEventArgs : EventArgs
+    {
+        private readonly string message;
+        public MessageEventArgs(string m)
+        {
+            message = m;
+        }
+        public string MessageNotify { get => message; }
+    }
     public delegate void TimerCount(int n);
     class Timer
     {
         private int seconds;
         public event TimerCount TimerCount;
-        public delegate void TimerNotify(string t);
-        public event TimerNotify Notify;
+        public event EventHandler<MessageEventArgs> Message;
         public Timer(int s)
         {
             seconds = s;
+        }
+        private void OnMessageSent(MessageEventArgs e)
+        {
+            EventHandler<MessageEventArgs> handler = Volatile.Read(ref Message);
+            handler?.Invoke(this, e);
         }
         public void CountBackward()
         {
             while (seconds > 0)
             {
-                TimerCount(seconds--);
+                TimerCount.Invoke(seconds--);
                 Thread.Sleep(1000);
             }
-            Notify.Invoke("It's time!");
+            MessageEventArgs messageEvent = new("It's time!");
+            OnMessageSent(messageEvent);
         }
     }
     class Program
@@ -35,7 +49,7 @@ namespace SleepEvent
         static void Main()
         {
             Timer timer = InputSeconds();
-            timer.Notify += t => Console.WriteLine(t);          //show message
+            timer.Message += Timer_Message;
             TimerCount timerCountMain = (n) =>
             {
                 Console.WriteLine(n);
@@ -51,6 +65,10 @@ namespace SleepEvent
             timer.TimerCount += Each10Sec;
             Console.WriteLine("Counting started...");
             timer.CountBackward();
+        }
+        private static void Timer_Message(object sender, MessageEventArgs e)
+        {
+            Console.WriteLine(e.MessageNotify);
         }
     }
 }
